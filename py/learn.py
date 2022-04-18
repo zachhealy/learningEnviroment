11b579ed043efb4ed347d56242ba1bb5f530af33
zachhealy <zachjhealy@gmail.com>
2022-04-17 22:17:17 -0500

    learning python again

---
+++ py/learn.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy
+
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+plt.scatter(x, y)
+plt.show()
