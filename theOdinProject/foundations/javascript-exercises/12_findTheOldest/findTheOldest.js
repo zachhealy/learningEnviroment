9b92bb6d31847c9d5bb4037516fcd67d1c022821
Zach Healy <zachjhealy@gmail.com>
2023-09-22 00:04:29 -0500

    finished foundations pt 5!

--- theOdinProject/foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ theOdinProject/foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = checkAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = checkAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        
+        if(Math.max(oldestAge, currentAge) === oldest){
+            return oldest.name;
+        } else {
+            return currentPerson.age;
+        }
+        
+    })
 
 };
+
+const checkAge = function(yearOfBirth, yearOfDeath) {
+    if(!yearOfDeath){
+        yearOfDeath = new Date.getFullYear();
+    }
+    return yearOfDeath - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
