254275d4f73c7cc4f104614400ea022174fa6231
Zach Healy <zachjhealy@gmail.com>
2023-09-25 22:30:19 -0500

    Update script.js

---
+++ theOdinProject/foundations/projects/calculator/script.js
@@ -0,0 +1,26 @@
+let num1 = 0;
+let num2 = 0;
+let oper = '';
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if(b === 0){
+        return 'undefined'
+    }
+    return a/b;
+}
+
+function operate(){
+
+}
